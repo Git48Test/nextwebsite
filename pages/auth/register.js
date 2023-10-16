b1207c303af23a3db35005ea1ac74027c4223e6c
Amer Alabdallah <amer@Amers-Air.fritz.box>
2023-10-16 08:34:04 +0200

    create login.js and register.js and _app.js and _document.js and index.js

---
+++ pages/auth/register.js
@@ -0,0 +1,99 @@
+// pages/auth/register.js
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function Register() {
+  // Create a form instance with validation rules
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // Handle form submission
+  async function onSubmit(data) {
+    // Call your API endpoint to register the user
+    // ...
+  }
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-full max-w-md p-4 space-y-8 bg-white rounded-md shadow-md">
+        <h1 className="text-2xl font-bold text-center">Create an account</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          <div>
+            <label htmlFor="name" className="block font-bold text-gray-700">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              {...register("name", {
+                required: "Name is required",
+              })}
+              placeholder="Enter your name"
+              className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
+            />
+            {errors.name && (
+              <p className="mt-1 text-sm text-red-600">{errors.name.message}</p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="email" className="block font-bold text-gray-700">
+              Email address
+            </label>
+            <input
+              id="email"
+              type="email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                },
+              })}
+              placeholder="Enter your email"
+              className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
+            />
+            {errors.email && (
+              <p className="mt-1 text-sm text-red-600">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="password" className="block font-bold text-gray-700">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+              })}
+              placeholder="Enter your password"
+              className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
+            />
+            {errors.password && (
+              <p className="mt-1 text-sm text-red-600">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full px-4 py-3 font-bold text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
